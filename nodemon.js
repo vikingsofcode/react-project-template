23ba79b89d1ad50cd881cd2324132a1f28b4a994
Niklas Lepisto <niklas.lepisto@idean.com>
2015-12-21 15:59:50 -0800

    Add nodemon

---
+++ nodemon.js
@@ -0,0 +1,11 @@
+{
+  "restartable": "rs",
+  "colours": true,
+  "ignore": [
+    ".git",
+    "node_modules/**/node_modules",
+    "assets/*"
+  ],
+  "watch": ['build/server/index.js', 'build/client/*.{js,jsx}']
+  "ext": "js json jsx"
+}
