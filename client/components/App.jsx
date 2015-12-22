7e2100f3807749e154f60d9dc313d41c5de23052
Niklas Lepisto <niklas.lepisto@idean.com>
2015-12-22 11:12:00 -0800

    Add the necessary welcome text

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default class App extends React.Component {
   render() {
-    return <h1>sdsdasdasdsdsdasd</h1>
+    return (
+      <p>
+        Lo there, do I see my father, <br />
+        Lo there, do I see my mother, my sisters and brothers, <br />
+        Lo there, do I see the line of my people, back to the beginning, <br />
+        Lo, they do call to me, they bid me take my place among them, <br />
+        In the halls of Valhalla where the brave may code forever.
+    </p>
+    )
   }
 }
